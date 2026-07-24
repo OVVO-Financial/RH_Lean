0525c50247e2c119516c553bb9110031922f9976
OVVO-Financial <ovvo.financial.systems@gmail.com>
2026-07-24 10:55:05 -0400

    Import concrete high residual data

--- RHLean.lean
+++ RHLean.lean
@@ -37,6 +37,7 @@ import RHLean.Geometry.SquareMapConformality
 import RHLean.Geometry.TwoABDisplacement
 import RHLean.Kernel.FixedPackets
 import RHLean.Proof.ConcreteSquarePrefixCofactorRealization
+import RHLean.Proof.ConcreteSquarePrefixHighResidual
 import RHLean.Proof.FareyModesAndTransportWindows
 import RHLean.Proof.NormalizedCofactorExpansion
 import RHLean.Proof.NormalizedCofactorTripling
