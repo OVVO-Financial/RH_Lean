c5f083d939a0dd814c32f1dbffe93004a8b655b0
OVVO-Financial <ovvo.financial.systems@gmail.com>
2026-07-27 03:30:44 -0400

    import concrete Li core-extension weight

--- RHLean.lean
+++ RHLean.lean
@@ -9,6 +9,7 @@ import RHLean.Analysis.CanonicalLowOccupancy
 import RHLean.Analysis.ComplexQuadraticPhase
 import RHLean.Analysis.ConcreteSquarePrefixGeometry
 import RHLean.Proof.ComplementaryMainGram
+import RHLean.Proof.ConcreteLiCoreExtensionWeight
 import RHLean.Proof.CoreExtensionDefect
 import RHLean.Proof.EndpointCubeAnalyticClosure
 import RHLean.Proof.EndpointProjectionBridge
