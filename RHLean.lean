bde46e8bf9848d79b84350f2f5e1594f6e519166
OVVO-Financial <ovvo.financial.systems@gmail.com>
2026-08-07 22:54:03 -0400

    Update Lean root import manifest

--- RHLean.lean
+++ RHLean.lean
@@ -19,6 +19,8 @@ import RHLean.Analysis.LogWeightedPrimeExtensionFiber
 import RHLean.Analysis.MathlibMertensHook
 import RHLean.Analysis.MertensEnergyCriterion
 import RHLean.Analysis.MertensPowerGrowth
+import RHLean.Analysis.MertensStepFunction
+import RHLean.Analysis.MertensStepAsymptotics
 import RHLean.Analysis.OptimalLogBase
 import RHLean.Analysis.OriginalVioleFunction
 import RHLean.Analysis.PrimeBoundaryDefectBridge
