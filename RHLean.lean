301704eb2c9e404e61fc3b2c804f5a3ae3872f0f
OVVO-Financial <ovvo.financial.systems@gmail.com>
2026-08-08 13:18:47 -0400

    Register survivor residue covariance module

--- RHLean.lean
+++ RHLean.lean
@@ -222,6 +222,7 @@ import RHLean.Proof.SquareBlockSmoothTransportGram
 import RHLean.Proof.SquareBlockTransportBaseline
 import RHLean.Proof.SquareRootAncestryRoot
 import RHLean.Proof.SquareRootAncestrySuccessor
+import RHLean.Proof.SurvivorResidueCovariance
 import RHLean.Proof.SurvivorZeroMode
 import RHLean.Proof.SymmetricPrimeExtension
 import RHLean.Proof.TerminalAxiomAudit
