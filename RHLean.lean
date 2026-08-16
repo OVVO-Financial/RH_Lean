69862218aed81058243de6ce50e2d6524729ddc0
OVVO-Financial <ovvo.financial.systems@gmail.com>
2026-08-16 16:44:53 -0400

    Import least-square defect channels

--- RHLean.lean
+++ RHLean.lean
@@ -123,6 +123,7 @@ import RHLean.Analysis.NativePNTTransfer
 import RHLean.Analysis.OptimalLogBase
 import RHLean.Analysis.OriginalVioleFunction
 import RHLean.Analysis.PRoughSquarePrefixEnergy
+import RHLean.Analysis.PhysicalDegreeOneLeastSquareChannels
 import RHLean.Analysis.PhysicalDegreeOneMixingConjecture
 import RHLean.Analysis.PhysicalDegreeOneTransitionEstimate
 import RHLean.Analysis.PrimeAveragedCubeEnergy
