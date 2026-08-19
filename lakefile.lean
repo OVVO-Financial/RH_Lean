5be5fb874499baa773ee469d112d0b659672fc7f
OVVO-Financial <ovvo.financial.systems@gmail.com>
2026-08-19 18:54:26 -0400

    test StrongPNT compatibility with Lean 4.24

--- lakefile.lean
+++ lakefile.lean
@@ -7,4 +7,7 @@ package «RHLean» where
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.24.0"
 
+require «StrongPNT» from git
+  "https://github.com/math-inc/strongpnt.git" @ "2f5835c322314f55f1026ec2f139d704b7c45c69"
+
 lean_lib RHLean
