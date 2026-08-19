47c8051b1b059a9fd7911c583589b6d0e5a4f41d
OVVO-Financial <ovvo.financial.systems@gmail.com>
2026-08-19 19:19:15 -0400

    probe Mathlib 4.32.2 compatibility

--- lakefile.lean
+++ lakefile.lean
@@ -5,6 +5,6 @@ package «RHLean» where
   version := v!"0.1.0"
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.24.0"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.32.2"
 
 lean_lib RHLean
