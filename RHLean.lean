143f3572e13ea57fce7dec5417b38c76a4efcfc2
OVVO-Financial <ovvo.financial.systems@gmail.com>
2026-09-07 12:04:49 -0400

    Import post-root lcm interior packing

--- RHLean.lean
+++ RHLean.lean
@@ -542,6 +542,7 @@ import RHLean.Proof.OneBlockInvariant
 import RHLean.Proof.OrderedEulerCutProjection
 import RHLean.Proof.OrthogonalResidual
 import RHLean.Proof.PostRootCovarianceLcmBoundary
+import RHLean.Proof.PostRootCovarianceLcmInteriorPacking
 import RHLean.Proof.PrefixCarrierOthelloWalls
 import RHLean.Proof.PrimeCombDiscrepancyRecurrence
 import RHLean.Proof.PrimeCombReciprocalBandCancellation
